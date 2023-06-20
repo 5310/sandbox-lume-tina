b09b628c582cfc8779367634f5d1927fe02e4298
Sayantan Chaudhuri <5310@users.noreply.github.com>
2023-06-20 13:54:49 +0000

    Rename the Lume config for clarity

--- lume_config.ts
+++ lume_config.ts
@@ -2,7 +2,9 @@ import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx.ts";
 import esbuild from "lume/plugins/esbuild.ts";
 
-const site = lume();
+const site = lume({
+  src: "./src",
+});
 
 site.use(jsx());
 site.use(esbuild(
